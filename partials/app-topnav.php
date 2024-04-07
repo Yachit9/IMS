0a9e854ed1385386301f98993e8a23faf520e3ce
YACHIT MAHAJAN <113870482+Yachit9@users.noreply.github.com>
2024-04-07 16:49:29 +0530

    Add files via upload

---
+++ partials/app-topnav.php
@@ -0,0 +1,4 @@
+<div class="dashboardtopnav">
+                <a href="" id="togglebtn"><i class="fa fa-navicon"></i></a>
+                <a href="database/logout.php" id="logoutbtn"><i class="fa fa-power-off">Log-Out</i></a>
+</div>
