68a772fcd1fa64668314c136bd6459dd8d998ace
YACHIT MAHAJAN <113870482+Yachit9@users.noreply.github.com>
2024-04-18 13:59:40 +0530

    Delete partials directory

--- partials/app-topnav.php
+++
@@ -1,4 +0,0 @@
-<div class="dashboardtopnav">
-                <a href="" id="togglebtn"><i class="fa fa-navicon"></i></a>
-                <a href="database/logout.php" id="logoutbtn"><i class="fa fa-power-off">Log-Out</i></a>
-</div>
